3eab11591ff715b3ede8671ff01d9215f38ef9ba
Thomas Bott <thomas_bott@outlook.de>
2023-03-28 14:00:52 +0200

    add promptspeech training pipelines

---
+++ TrainingInterfaces/TrainingPipelines/PortaSpeech_PromptSpeech.py
@@ -0,0 +1,70 @@
+import time
+
+import torch
+import wandb
+
+from TrainingInterfaces.Text_to_Spectrogram.PortaSpeech.PortaSpeech import PortaSpeech
+from TrainingInterfaces.Text_to_Spectrogram.PortaSpeech.portaspeech_train_loop_arbiter import train_loop
+from Utility.corpus_preparation import prepare_fastspeech_corpus
+from Utility.path_to_transcript_dicts import *
+from Utility.storage_config import MODELS_DIR
+from Utility.storage_config import PREPROCESSING_DIR
+
+import sys
+
+
+def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb_resume_id):
+    if gpu_id == "cpu":
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        device = torch.device("cpu")
+
+    else:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_id}"
+        device = torch.device("cuda")
+
+    torch.manual_seed(131714)
+    random.seed(131714)
+    torch.random.manual_seed(131714)
+
+    print("Preparing")
+
+    name = "04_PortaSpeech_PromptSpeech"
+
+    if model_dir is not None:
+        save_dir = model_dir
+    else:
+        save_dir = os.path.join(MODELS_DIR, name)
+    os.makedirs(save_dir, exist_ok=True)
+
+    train_set = prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_promptspeech(),
+                                          corpus_dir=os.path.join(PREPROCESSING_DIR, "promptspeech"),
+                                          lang="en",
+                                          save_imgs=False)
+    
+    sys.exit()
+
+    model = PortaSpeech()
+    if use_wandb:
+        wandb.init(
+            name=f"{name}_{time.strftime('%Y%m%d-%H%M%S')}" if wandb_resume_id is None else None,
+            id=wandb_resume_id,  # this is None if not specified in the command line arguments.
+            resume="must" if wandb_resume_id is not None else None)
+    print("Training model")
+    train_loop(net=model,
+               datasets=[train_set],
+               device=device,
+               save_directory=save_dir,
+               batch_size=8,
+               eval_lang="en",
+               path_to_checkpoint=resume_checkpoint,
+               path_to_embed_model=os.path.join(MODELS_DIR, "Embedding", "embedding_function.pt"),
+               fine_tune=finetune,
+               resume=resume,
+               use_wandb=use_wandb,
+               warmup_steps=8000,
+               postnet_start_steps=9000,
+               phase_1_steps=80000,
+               phase_2_steps=0)
+    if use_wandb:
+        wandb.finish()
