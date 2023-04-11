c39a3f5bff5ca8286d9b3e4fd8cac2578107de25
Thomas Bott <thomas_bott@outlook.de>
2023-04-11 13:07:59 +0200

    scorer testing

--- run_scorer.py
+++ run_scorer.py
@@ -12,13 +12,17 @@ from Utility.Scorer import TTSScorer
 from Utility.storage_config import MODELS_DIR
 from Utility.storage_config import PREPROCESSING_DIR
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = f"5"
 exec_device = "cuda" if torch.cuda.is_available() else "cpu"
 
-alignment_scorer = AlignmentScorer(path_to_aligner_model=os.path.join(MODELS_DIR, "Aligner", "aligner.pt"), device=exec_device)
-alignment_scorer.score(path_to_aligner_dataset=os.path.join(PREPROCESSING_DIR, "IntegrationTest", "aligner_train_cache.pt"))
-alignment_scorer.show_samples_with_highest_loss(20)
+#alignment_scorer = AlignmentScorer(path_to_aligner_model=os.path.join(MODELS_DIR, "Aligner", "aligner.pt"), device=exec_device)
+#alignment_scorer.score(path_to_aligner_dataset=os.path.join(PREPROCESSING_DIR, "blizzard2013", "aligner_train_cache.pt"))
+#alignment_scorer.save_scores()
+#alignment_scorer.show_samples_with_highest_loss(n=50)
+#alignment_scorer.remove_samples_with_highest_loss(path_to_aligner_dataset=os.path.join(PREPROCESSING_DIR, "blizzard2013", "aligner_train_cache.pt"), n=20)
 
-tts_scorer = TTSScorer(path_to_portaspeech_model=os.path.join(MODELS_DIR, "FastSpeech2_IntegrationTest", "best.pt"), device=exec_device)
-tts_scorer.score(path_to_portaspeech_dataset=os.path.join(PREPROCESSING_DIR, "IntegrationTest/"), lang_id="en")
+tts_scorer = TTSScorer(path_to_portaspeech_model=os.path.join(MODELS_DIR, "05_PortaSpeech_PromptSpeech", "best.pt"), device=exec_device)
+tts_scorer.score(path_to_portaspeech_dataset=os.path.join(PREPROCESSING_DIR, "promptspeech"), lang_id="en")
 tts_scorer.show_samples_with_highest_loss(20)
 tts_scorer.remove_samples_with_highest_loss(20)
